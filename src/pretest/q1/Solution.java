87dcfaa3047aba796f8c14daebee3abc9ebbf3da
Macros Zhang <zhangtuo102@163.com>
2017-03-16 18:12:54 +0800

    solution1

--- src/pretest/q1/Solution.java
+++ src/pretest/q1/Solution.java
@@ -3,15 +3,44 @@ package pretest.q1;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class Solution {
 	public  Set<String> findWords(Set<String> wordSet){
 		Set<String> set = new HashSet<>();
-		set = wordSet;
+		for (String word : wordSet) {
+			if(isOneLine4KeyBoard(word)){
+				set.add(word);
+			}
+		}
 		return set;
 	}
-	
-	
-	
+	private boolean isOneLine4KeyBoard(String word){
+		boolean flag = true;
+		String wordUC = word.toUpperCase();
+		char first = wordUC.charAt(0);
+		if("QWERTYUIOP".indexOf(first)>-1){
+			for(int i=1;i<word.length();i++){
+				if ("QWERTYUIOP".indexOf(wordUC.charAt(i))==-1) {
+					flag = false;
+				}
+			}
+		}
+		if("ASDFGHJKL".indexOf(first)>-1){
+			for(int i=1;i<word.length();i++){
+				if ("ASDFGHJKL".indexOf(wordUC.charAt(i))==-1) {
+					flag = false;
+				}
+			}
+		}
+		if("ZXCVBNM".indexOf(first)>-1){
+			for(int i=1;i<word.length();i++){
+				if ("ZXCVBNM".indexOf(wordUC.charAt(i))==-1) {
+					flag = false;
+				}
+			}
+		}
+		return flag;
+	}	
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		Set<String> set = new HashSet<>();
---
+++ src/pretest/q2/Solution.java
@@ -0,0 +1,21 @@
+package pretest.q2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+	private class Node{
+		public Node left;
+		public Node right;
+		public int value;
+	}
+	public List<Node> findPath(Node rootNode,int sum){
+		List<Node> nodes = new ArrayList<>();
+		
+		return nodes;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
